8c9f9e0a50368854fed2ffaf6ec184e260488464
Amir <62354732+NoA2647@users.noreply.github.com>
2021-04-04 21:13:26 +0430

    Add files via upload
    calculate order and steps

--- A-star-find-path.py
+++ A-star-find-path.py
@@ -38,59 +38,50 @@ def calculateHCost(i, j, nodeEnd):
 
 def up(node, nodeEnd):
     if node.i > 0:
-        print("start up")
         nodeUp = matrix[node.i - 1][node.j]
         if nodeUp.value == 0 and not nodeUp.check:
             nodeUp.gCost = node.gCost + cost
             nodeUp.hCost = calculateHCost(nodeUp.i, nodeUp.j, nodeEnd)
             nodeUp.parent = node
             neighbors.append(nodeUp)
-            print("nodeUp add")
 
 
 def down(node, nodeEnd):
     if node.i < 9:
-        print("start down")
         nodeDown = matrix[node.i + 1][node.j]
         if nodeDown.value == 0 and not nodeDown.check:
             nodeDown.gCost = node.gCost + cost
             nodeDown.hCost = calculateHCost(nodeDown.i, nodeDown.j, nodeEnd)
             nodeDown.parent = node
             neighbors.append(nodeDown)
-            print("nodeDown add")
 
 
 def right(node, nodeEnd):
     if node.j < 9:
-        print("start right")
         nodeRight = matrix[node.i][node.j + 1]
         if nodeRight.value == 0 and not nodeRight.check:
             nodeRight.gCost = node.gCost + cost
             nodeRight.hCost = calculateHCost(nodeRight.i, nodeRight.j, nodeEnd)
             nodeRight.parent = node
             neighbors.append(nodeRight)
-            print("nodeRight add")
 
 
 def left(node, nodeEnd):
     if node.j > 0:
-        print("start left")
         nodeLeft = matrix[node.i][node.j - 1]
         if nodeLeft.value == 0 and not nodeLeft.check:
             nodeLeft.gCost = node.gCost + cost
             nodeLeft.hCost = calculateHCost(nodeLeft.i, nodeLeft.j, nodeEnd)
             nodeLeft.parent = node
             neighbors.append(nodeLeft)
-            print("nodeLeft add")
 
 
 def aStar(nodeStart, nodeEnd):
-    level = 0
+    order = 0
     while True:
-        level += 1
-        print("level:", level, "position:", nodeStart.i, nodeStart.j)
+        order += 1
         if nodeStart.i == nodeEnd.i and nodeStart.j == nodeEnd.j:
-            return nodeStart
+            return nodeStart, order
         up(nodeStart, nodeEnd)
         down(nodeStart, nodeEnd)
         right(nodeStart, nodeEnd)
@@ -107,29 +98,33 @@ def aStar(nodeStart, nodeEnd):
 
 
 def start():
-    startI = 0  # int(input("Enter I index of start: "))
-    startJ = 0  # int(input("Enter J index of start: "))
-    endI = 9  # int(input("Enter I index of end: "))
-    endJ = 9  # int(input("Enter J index of end: "))
+    startI = int(input("Enter I index of start: "))
+    startJ = int(input("Enter J index of start: "))
+    endI = int(input("Enter I index of end: "))
+    endJ = int(input("Enter J index of end: "))
     readMap()
     nodeStart = matrix[startI][startJ]
     nodeEnd = matrix[endI][endJ]
     nodeStart.hCost = calculateHCost(nodeStart.i, nodeStart.j, nodeEnd)
-    node = aStar(nodeStart, nodeEnd)
+    node, order = aStar(nodeStart, nodeEnd)
+    steps = 0
     path = list()
     if node:
         while node is not None:
+            steps += 1
             path.append((node.i, node.j))
             node = node.parent
+        print("order:", order)
+        print("steps:", steps)
         for i in range(10):
             for j in range(10):
                 if path.count((i, j)) == 0:
-                    print(Fore.RESET,matrix[i][j].value,end=" ")
+                    print(Fore.RESET, matrix[i][j].value, end=" ")
                 else:
-                    print(Fore.GREEN,matrix[i][j].value,end=" ")
+                    print(Fore.GREEN, matrix[i][j].value, end=" ")
             print("\n")
     else:
-        print(Fore.RED+"no solution")
+        print(Fore.RED + "no solution")
     pass
 
 
